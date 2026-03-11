903cfc859c5585a79bd71b120b3cae038be6fd8a
Your Name <you@example.com>
2026-03-11 16:38:14 -0700

    merge sieve impl

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "cache_runner.h"
 #include "fifo.h"
 #include "lru.h"
+#include "sieve.h"
 
 const std::string twi_trace_location = "twi_proc/";
 int main() {
@@ -10,7 +11,7 @@ int main() {
   std::vector<double> prop{0.01};
   Bench b(threads_choices, prop, scale_policy::TRANSFORM_SPACE);
   b.add_cache<FIFO>("FIFO");
-  b.add_cache<LRU>("LRU");
+  b.add_cache<SIEVE>("SIEVE");
   // b.add_cache<BadCache>("BadCache");
   b.add_trace("twitter", twi_trace_location, 1);
   b.run();
---
+++ sieve.h
@@ -0,0 +1,104 @@
+#pragma once
+#include "cache.h"
+#include <cassert>
+#include <functional>
+#include <list>
+#include <mutex>
+#include <unordered_map>
+
+struct Node {
+  cache_key_t key;
+  cache_token_t token;
+  bool visited = false;
+};
+
+class SIEVE : public ICache {
+private:
+  size_t cap;
+  std::list<Node> nodeList;
+  std::unordered_map<cache_key_t, std::list<Node>::iterator> map;
+  std::list<Node>::iterator hand = nodeList.end();
+  std::mutex mut;
+
+  bool in(cache_key_t key) const { return map.contains(key); }
+  bool can_add() const { return map.size() < cap; }
+
+  void advance_hand() {
+    if (nodeList.empty()) {
+      hand = nodeList.end();
+      return;
+    }
+
+    if (hand == nodeList.end()) {
+      hand = std::prev(nodeList.end()); // start at back
+      return;
+    }
+
+    if (hand == nodeList.begin()) {
+      hand = std::prev(nodeList.end()); // wrap front -> back
+      return;
+    }
+
+    --hand;
+  }
+
+  void add(cache_key_t key, cache_token_t token) {
+    assert(nodeList.size() < cap);
+
+    nodeList.push_front(Node{key, token, false});
+    map[key] = nodeList.begin();
+
+    if (nodeList.size() == 1)
+      hand = nodeList.begin();
+  }
+
+  void evict() {
+    while (true) {
+      if (hand == nodeList.end())
+        hand = nodeList.begin();
+
+      if (!hand->visited) {
+        auto to_remove = hand;
+        advance_hand();
+        map.erase(to_remove->key);
+        nodeList.erase(to_remove);
+
+        if (nodeList.empty())
+          hand = nodeList.end();
+
+        return;
+      }
+
+      hand->visited = false;
+      advance_hand();
+    }
+  }
+
+public:
+  explicit SIEVE(size_t cap) : cap(cap) { assert(cap > 0); }
+
+  cache_token_t
+  query(cache_key_t k,
+        std::function<cache_token_t(cache_key_t)> get_token) override {
+    // lock while we look for element
+    std::lock_guard<std::mutex> lock(mut);
+
+    // can this hot path avoid a lock??
+    auto it = map.find(k);
+    if (it != map.end()) {
+      it->second->visited = true;
+      return it->second->token;
+    }
+
+    cache_token_t t = get_token(k);
+
+    // evict according to docs
+    if (!can_add())
+      evict();
+
+    add(k, t);
+    return t;
+  }
+
+  size_t get_cap() const override { return cap; }
+};
