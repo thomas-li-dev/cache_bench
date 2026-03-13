1263ca8d9412aabf10f055342ebbefe93bfb5492
William Hu <willhu@stanford.edu>
2026-03-13 12:53:17 -0400

    port sieve implementation

--- analysis/cache_algos/sieve.py
+++ analysis/cache_algos/sieve.py
@@ -4,28 +4,86 @@ from collections import OrderedDict
 
 from . import _register
 from .base import CacheAlgo
+from threading import RLock
+
+class SIEVENode:
+    def __init__(self, value):
+        self.value = value
+        self.visited = False
+        self.next = None
+        self.prev = None
+
+        self.lock = RLock()
 
 @_register("sieve")
 class Sieve(CacheAlgo):
 
     def __init__(self, cache_size: int):
         super().__init__(cache_size)
-        self._contents: OrderedDict[int, bool] = OrderedDict()
+
+        # Store hashmap and doubly linked list of data
+        self.cache = {}
+        self.head = None
+        self.tail = None
+        
+        self.hand = None
+        self.size = 0
+
+        self.rlock = RLock()
 
     def get(self, obj_id: int) -> bool:
-        self._num_requests += 1
+        with self.rlock:
+            self._num_requests += 1
+            # Item in cache, set visited flag
+            if obj_id in self.cache: 
+                self.cache[obj_id].visited = True
+                return True
 
-        if obj_id in self._contents:
-            self._contents[obj_id] = True
-            return True
+            if self.cache_size == 0:
+                self._num_misses += 1
+                return False
 
-        self._num_misses += 1
+            self._num_misses += 1
+            # if our cache is full, evict an element to make space 
+            if self.size == self.cache_size:
+                self._evict()
 
-        while len(self._contents) >= self.cache_size:
-            victim_id, victim_visited = self._contents.popitem(last=False)
-            if victim_visited:
-                self._contents[victim_id] = False
-                continue
-        
-        self._contents[obj_id] = False
-        return False
+            node = SIEVENode(obj_id)
+            node.next = self.head
+            node.prev = None
+            if self.head:
+                self.head.prev = node
+            self.head = node
+            if self.tail is None:
+                self.tail = node
+
+            self.cache[obj_id] = node
+            self.size += 1
+            node.visited = False 
+
+            return False
+
+    def _evict(self):
+        with self.rlock:
+            to_remove = self.hand if self.hand else self.tail
+            while(to_remove != None and to_remove.visited == True):
+                # Unset visited
+                to_remove.visited = False
+                to_remove = to_remove.prev if to_remove.prev else self.tail
+            if(to_remove == None):
+                # We should never reach this but just in case
+                return
+            self.hand = to_remove.prev if to_remove.prev else None
+            
+            # Remove the evicted element from our cache
+            del self.cache[to_remove.value]
+            if to_remove.prev:
+                to_remove.prev.next = to_remove.next
+            else:
+                self.head = to_remove.next
+            if to_remove.next:
+                to_remove.next.prev = to_remove.prev
+            else:
+                self.tail = to_remove.prev
+            
+            self.size -= 1
