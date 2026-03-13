2cc01135227819065010cb864bd28471ea16515d
Your Name <you@example.com>
2026-03-13 15:50:52 -0700

    no allocation

--- sieve_bit.h
+++ sieve_bit.h
@@ -30,6 +30,11 @@ private:
       in[i / W] &= ~(T(1) << (i % W));
       num_in--;
     }
+    void reset() {
+      memset(vis, 0, sizeof vis);
+      memset(in, 0, sizeof in);
+      num_in = 0;
+    }
     bool empty() { return num_in == 0; }
     int nxt_and_clr(int st) {
       for (int b = st / W; b < K; b++) {
@@ -60,29 +65,31 @@ private:
   };
   struct ListData {
     Block b;
-    ListData *nxt, *prv;
+    int nxt, prv;
   };
   struct MapData {
     Block *b;
     cache_token_t t;
     size_t i;
   };
-  ListData *head, *hand_l;
-  size_t hand_i{};
+  int head{}, hand_l{};
+  int hand_i{};
   size_t head_num = 0;
   int64_t cap;
   int64_t siz{};
   unordered_flat_map<cache_key_t, MapData> map;
 
-  std::queue<ListData *> free;
+  std::vector<ListData> buf;
+  std::vector<int> free;
 
 public:
-  explicit SIEVEBit(size_t cap) : cap(cap) {
+  explicit SIEVEBit(size_t cap) : cap(cap), buf(cap) {
     assert(cap > 0);
-    head = new ListData{{}, nullptr, nullptr};
-    head->nxt = head->prv = head;
+    assert(cap < 1e9);
     hand_l = head;
     hand_i = 0;
+    for (size_t i = 1; i < cap; i++)
+      free.push_back(i);
   }
 
   cache_token_t query(cache_key_t k, cache_token_t (*get_token)(void *),
@@ -94,23 +101,23 @@ public:
       dat.b->set_vis(dat.i);
       return dat.t;
     }
-    if (siz == cap) {
+    while (siz >= cap) {
       // evict smth
       if (hand_i == W * K) {
-        hand_l = hand_l->prv;
+        hand_l = buf[hand_l].prv;
         hand_i = 0;
       }
-      hand_i = hand_l->b.nxt_and_clr(hand_i);
+      hand_i = buf[hand_l].b.nxt_and_clr(hand_i);
       if (hand_i != W * K) {
-        hand_l->b.clr_in(hand_i);
-        map.erase(hand_l->b.ks[hand_i]);
+        buf[hand_l].b.clr_in(hand_i);
+        map.erase(buf[hand_l].b.ks[hand_i]);
         siz--;
         hand_i++;
-        if (hand_l != head && hand_l->b.empty()) {
-          auto prv = hand_l->prv, nxt = hand_l->nxt;
-          prv->nxt = nxt;
-          nxt->prv = prv;
-          free.push(hand_l);
+        if (hand_l != head && buf[hand_l].b.empty()) {
+          auto prv = buf[hand_l].prv, nxt = buf[hand_l].nxt;
+          buf[prv].nxt = nxt;
+          buf[nxt].prv = prv;
+          free.push_back(hand_l);
           hand_l = prv;
           hand_i = 0;
         }
@@ -119,27 +126,26 @@ public:
     t = get_token(ctx);
     if (head_num == W * K) {
       // make new head
-      ListData *nhead;
-      if (free.size()) {
-        nhead = free.front();
-        free.pop();
-      } else
-        nhead = new ListData;
-      auto prv = head->prv, nxt = head;
-      prv->nxt = nhead;
-      nhead->prv = prv;
-      nxt->prv = nhead;
-      nhead->nxt = nxt;
-
+      int nhead;
+      assert(free.size());
+      nhead = free.back();
+      free.pop_back();
+      buf[nhead].b.reset();
+      auto prv = buf[head].prv, nxt = head;
+      buf[prv].nxt = nhead;
+      buf[nhead].prv = prv;
+      buf[nxt].prv = nhead;
+      buf[nhead].nxt = nxt;
       head = nhead;
       head_num = 0;
     }
     // put into head
-    head->b.set_in(head_num);
-    head->b.ks[head_num] = k;
-    map[k] = {&head->b, t, head_num};
+    buf[head].b.set_in(head_num);
+    buf[head].b.ks[head_num] = k;
+    map[k] = {&buf[head].b, t, head_num};
     head_num++;
     siz++;
+    assert(map.size() <= cap);
 
     return t;
   }
